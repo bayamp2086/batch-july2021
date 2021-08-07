7ce1685277b4595df3c8fc3950d2909b6b60b212
bayamp2086 <Bayamp2015!>
2021-08-06 20:28:57 -0700

    Time pass

---
+++ myjavaproject/src/com/bayamp/training/array/sandhya/programs/Test.java
@@ -0,0 +1,10 @@
+package com.bayamp.training.array.sandhya.programs;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
