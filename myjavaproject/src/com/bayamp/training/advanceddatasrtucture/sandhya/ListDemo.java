bb3645bcdefe9f1147f1a7f5c37bf224d1fd7213
Sandhya E <sandhya.eduri8@gmail.com>
2021-08-11 15:57:35 -0700

    1. ListDemo

---
+++ myjavaproject/src/com/bayamp/training/advanceddatasrtucture/sandhya/ListDemo.java
@@ -0,0 +1,17 @@
+package com.bayamp.training.advanceddatasrtucture.sandhya;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] infantAges ={0,1,2,3,4,5};
+		
+		List ages = new ArrayList();
+		
+		//List is an interface and arrayList is an implementation
+	}
+
+}
