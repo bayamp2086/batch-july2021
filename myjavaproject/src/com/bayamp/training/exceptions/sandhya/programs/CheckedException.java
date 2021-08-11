f7b7a5db23e71389094b3c643529857d8c32c2f3
Sandhya E <sandhya.eduri8@gmail.com>
2021-08-11 15:30:45 -0700

    1. updated CheckedException 2. updated Stringutil

--- myjavaproject/src/com/bayamp/training/exceptions/sandhya/programs/CheckedException.java
+++ myjavaproject/src/com/bayamp/training/exceptions/sandhya/programs/CheckedException.java
@@ -1,13 +1,35 @@
 package com.bayamp.training.exceptions.sandhya.programs;
-import com.bayamp.training.utils.StringUtil;
+
+import com.bayamp.training.strinUtil.sandhya.StringUtil;
+
 public class CheckedException {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String str1 = null; 
+		String str2 = "bayamp";
+		
+		String reversedString1;
+		String reversedString2;
+		
+		try {
+			reversedString1 = StringUtil.reverse(str1);
+			System.out.println(reversedString1);
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			System.out.println("found an Exception");
+		}
+		
 
-		String str = StringUtil.reverse("bayamp");
-		
-		
+		try {
+			reversedString2 = StringUtil.reverse(str2);
+			System.out.println(reversedString2);
+			
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			System.out.println("found an Exception");
+		}
 	}
+
 
 }
--- myjavaproject/src/com/bayamp/training/strinUtil/sandhya/StringUtil.java
+++ myjavaproject/src/com/bayamp/training/strinUtil/sandhya/StringUtil.java
@@ -2,4 +2,13 @@ package com.bayamp.training.strinUtil.sandhya;
 
 public class StringUtil {
 
+	public static String reverse(String str) throws Exception {
+		
+		if (str == null)  {
+			throw new Exception("String can not be null");
+		}
+		
+		StringBuffer buff = new StringBuffer(str);
+		return buff.reverse().toString();
+	}
 }
