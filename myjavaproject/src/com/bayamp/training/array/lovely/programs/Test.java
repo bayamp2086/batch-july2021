4cf2cf707c1dca1d946ae5358d7835b02be5f202
Harish Appannagari <github@bayamp.com>
2021-08-11 18:50:47 -0700

    no message

---
+++ myjavaproject/src/com/bayamp/training/array/lovely/programs/Test.java
@@ -0,0 +1,10 @@
+package com.bayamp.training.array.lovely.programs;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- myjavaproject/src/com/bayamp/training/generic/programs/MyHelloWorld.java
+++ myjavaproject/src/com/bayamp/training/generic/programs/MyHelloWorld.java
@@ -6,6 +6,8 @@ public class MyHelloWorld {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello World");
+		
+		
 	}
 	
 
